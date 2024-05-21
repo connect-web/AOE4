7e183b0858a18ea4b2d2f5c235736d0dcd83590e
Elliot <141949359+webElliot@users.noreply.github.com>
2024-05-21 17:17:29 +0100

    application added to main

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
+from app import (
+    ResourceWidget, DataframeNavigator, MinimizeWidget,
+    NavigationButtons, OverlayWidget, css_cache,
+    get_dataframe
+)
+from PyQt6.QtWidgets import QApplication
 import sys
-import os
 
 
-from PyQt6.QtCore import Qt
-import sys
-from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QColor
+class Resources(ResourceWidget):
+    default_values = {'food': "", "wood": "", "stone": "", "gold": ""}
 
-from app.resources import Resources
+    def __init__(self):
+        super().__init__()
+        self.df_manager = DataframeNavigator(df=get_dataframe('ottomans_2.html'))
+        self.app = QApplication(sys.argv)
+        self.navigationButtons = NavigationButtons(self)
+        self.description = OverlayWidget(text=f"Loaded ottomans",
+                                         size=(10, 200, 400, 200),
+                                         css=css_cache.load('description.css')
+                                         )
+        self.update_map(self.default_values)
+        self.MinimizeWidget = MinimizeWidget(self, css=css_cache.load('Minimize.css'))
 
 
 
+    def back(self):
+        self.update_overlays(self.df_manager.back())
 
+    def next(self):
+        self.update_overlays(self.df_manager.next())
+
+    def update_overlays(self, update_dict):
+        self.update_map(update_dict)
+        self.description.setText(
+            update_dict.get('description', '')
+        )
+
+    def run(self):
+        sys.exit(self.app.exec())
 
 
 if __name__ == "__main__":
